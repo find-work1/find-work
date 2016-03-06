4fb69d99042728984000114d6c51372c1f20b7e2
maxpleaner <maxpleaner@gmail.com>
2016-03-06 04:00:47 -0800

    compile

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   def sample
   end
   def main
+    render "main", layout: false
   end
   def create_user
     websocket_response(User.create, "create")
